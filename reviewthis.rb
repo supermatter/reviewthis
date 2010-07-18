fc4123d044505d68dd5d0e32612fb5971582b1ef
tonydewan <tony@supermatter.com>
2010-07-18 00:35:58 -0400

    Basic first version.
    If a commit message includes the hash #reviewthis, any github user
    (signified by @username) will get an email (if they have a public
    email address). Also, any email address includes gets an email as
    well.
    
    Currently, sendmail is used in dev, and since we're going to deploy
    to heroku, the sendgrid addon is used in production.

--- reviewthis.rb
+++ reviewthis.rb
@@ -1,10 +1,49 @@
 require 'rubygems'
 require 'sinatra'
 require 'json'
+require 'mustache/sinatra'
+require 'octopi'
+require 'pony'
 
+configure do
+  include Octopi
+  
+  set :mustache, {
+     :views     => 'views/',
+     :templates => 'templates/'
+   }
+   
+  # regex's
+  USER = /[^a-z0-9_]@([a-z0-9_]+)/i
+  HASH = /[^a-z0-9_]#([a-z0-9_]+)/i
+  REVIEW = /[^a-z0-9_](#reviewthis)[^a-z0-9_]+/i
+  EMAIL = /\b([A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4})\b/i
+end
 
 configure :production do
   # only run om Heroku
+  set :via, :smtp
+  set :via_settings, {
+    :address        => "smtp.sendgrid.net",
+    :port           => "25",
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => ENV['SENDGRID_DOMAIN'],
+  }
+  
+end
+
+configure :development, :test do
+  set :via, :sendmail
+  set :via_settings, {}
+end
+
+helpers do
+  def mail(params)
+    body = mustache :email, {}, params
+    Pony.mail(:to => params[:email], :from => 'info@reviewthis.heroku.com', :subject => "Code Review Request: from #{params[:commit_author]}", :body => body, :via => options.via, :via_settings => options.via_settings) 
+  end
 end
 
 # test!
@@ -12,7 +51,36 @@ get '/' do
   "Reviewthis @github!"
 end
 
+# the meat
 post '/' do
   push = JSON.parse(params[:payload])
-  "Commit message: #{push['commits'][0]['message']}"
+  commit = push['commits'][0]
+  message = commit['message']
+
+  if message.match(REVIEW)
+    
+    # set some template vars
+    vars = {
+      :message => message,
+      :commit_author => commit['author']['name'],
+      :commit_url => commit['url'],
+      :repo_name => push['repository']['name']
+    }
+    
+    message.scan(USER) { |username|
+      user = User.find(username) #github user info!
+      vars[:username] = user.name
+      vars[:email] = user.email
+      mail(vars)
+    }
+  
+    message.scan(EMAIL) { |email|
+      vars[:username] = email
+      vars[:email] = email
+      mail(vars)
+    }
+  
+  end
+  
+  return
 end
